18b5864064f5197bf16c95e9c32e6d002b861aed
Nurjigit <sagendikov45@gmail.com>
2022-01-26 17:53:41 +0600

    Добавил user в админку

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Movie, MovieImage
+from .models import Movie, MovieImage, Profile
+
 
 
 admin.site.register(Movie)
 admin.site.register(MovieImage)
+admin.site.register(Profile)
